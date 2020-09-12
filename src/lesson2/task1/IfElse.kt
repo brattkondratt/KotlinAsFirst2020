f414d133d66e4012520a899ce8919dc6bc28d656
Andrey <kondratenko.av@edu.spbstu.ru>
2020-09-12 15:32:54 +0400

    2 lesson

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -103,7 +103,9 @@ fun whichRookThreatens(
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
 ): Int {
-    return if (((kingX == rookX1) || (kingY == rookY1)) && ((kingX != rookX2) && (kingY != rookY2)))
+    return if ((kingX != rookX1) && (kingY != rookY1) && ((kingX != rookX2) && (kingY != rookY2)))
+        return 0
+    else if (((kingX == rookX1) || (kingY == rookY1)) && ((kingX != rookX2) && (kingY != rookY2)))
         return 1
     else if (((kingX == rookX1) || (kingY == rookY1)) && ((kingX != rookX2) && (kingY != rookY2)))
         return 2
